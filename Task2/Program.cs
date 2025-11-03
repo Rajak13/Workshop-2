f9ac4950a53aefd9a3386332b4d0eb5def36ceda
Rajak13 <queresma05@gmail.com>
2025-11-03 12:07:46 +0545

    Modified task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,17 +1,11 @@
-﻿using System;
-
-public class Circle
+﻿public class Circle
 {
-    // Constant PI
     public const double PI = 3.14;
 
-    // Method to calculate area
     public double GetArea(double rad)
     {
         return PI * rad * rad;
     }
-
-    // Method to calculate perimeter
     public double GetPerimeter(double rad)
     {
         return 2 * PI * rad;
@@ -28,7 +22,6 @@ class Program
         Console.WriteLine("Area: " + circle.GetArea(radius));
         Console.WriteLine("Perimeter: " + circle.GetPerimeter(radius));
 
-        // Trying to modify PI (uncomment to see compilation error)
-        // Circle.PI = 3.14159; // Error: The left-hand side of an assignment must be a variable, property or indexer
+        // Trying to modify PI in public class gives Error: The left-hand side of an assignment must be a variable, property or indexer
     }
 }
